2753558fc74dd01bdc8eb505f435307d48f2c299
VyT <vytran1805@gmail.com>
2023-06-15 17:45:08 -0400

    Finish implementing data operations (GET, POST, PATCH, DELETE) for the task

--- api/app.js
+++ api/app.js
@@ -10,11 +10,10 @@ const { List, Task } = require("./db/models");
 /* LOAD MIDDLEWARE BODY-PARSER */
 // instructing Express to use body-parser to parse JSON payloads in the request body
 app.use(bodyParser.json());
-/* ROUTE HANDLERS */
-
-/* LIST ROUTES*/
-// sets up a route handler for the HTTP GET request on the root URL ("/")
-
+/********************************* ROUTE HANDLERS *************************************************/
+/*********************
+ *    LIST ROUTES    *
+ ********************/
 /**
  * GET /lists
  * Purpose: Get all lists
@@ -28,8 +27,17 @@ app.get("/lists", (req, res) => {
   });
 });
 
-/**
- * POST /lists
+/** GET one list with specific id /lists
+ * Purpose: Get all lists
+ * Action: It will return an array of all the lists in the database
+ */
+app.get("/lists/:listId", (req, res) => {
+  List.findOne({ _id: req.params.listId }).then((list) => {
+    res.send(list);
+  });
+});
+
+/** POST /lists
  * Purpose: Create a list
  * Action: Create a new list and return the new list back to the user, including the id
  */
@@ -53,7 +61,7 @@ app.post("/lists", (req, res) => {
 app.patch("/lists/:id", (req, res) => {
   // The list information (fields) will be passed in via the JSON request body
   // pass in the id, update the list that it found using the first param as the condition, with the req.body obj
-  List.findOneAndUpdate({ _id: req.params.id }, { $set: req.body }).then(
+  List.findOneAndUpdate({ _id: req.params.id, $set: req.body }).then(
     (updatedList) => {
       res.sendStatus(200); //send the status code of 200 which corresponds to a successful request.
       // res.send(updatedList);
@@ -67,11 +75,90 @@ app.patch("/lists/:id", (req, res) => {
  * Action: delete the record with the provided id in the url
  */
 app.delete("/lists/:id", (req, res) => {
-  List.findOneAndRemove({ _id: req.params.id }, { $set: req.body }).then((removedList) => {
-    res.send(removedList);
+  List.findOneAndRemove({ _id: req.params.id, $set: req.body }).then(
+    (removedList) => {
+      res.send(removedList);
+    }
+  );
+});
+/** This is the end of List routes **/
+
+/*********************
+ *    TASK ROUTES    *
+ ********************/
+/** GET all the task /lists/tasks
+ * Purpose: Get all tasks from list
+ * Action: It will return an array of all the tasks with the specific listId in the database
+ */
+app.get("/lists/:listId/tasks", (req, res) => {
+  // Express route handler using the HTTP GET method: get all the docs from tasks then res to the client
+  // Pass in the listId as a condition, fetches all tasks with that listId
+  Task.find({ _listId: req.params.listId }).then((task) => {
+    res.send(task);
   });
 });
 
+/** GET one specific task from id and listId /lists/:listId/tasks/:id
+ * Purpose: Get one specific task from id and listId
+ * Action: It will return a task with specific id and listId in the database
+ */
+app.get("/lists/:listId/tasks/:taskId", (req, res) => {
+  // Express route handler using the HTTP GET method: get all the docs from tasks then res to the client
+  // Pass in the listId as a condition, fetches all tasks with that listId
+  Task.findOne({ _listId: req.params.listId, _id: req.params.taskId }).then(
+    (task) => {
+      res.send(task);
+    }
+  );
+});
+
+/** POST /lists/:listId/tasks
+ * Purpose: Create a task that associate with the passed in listID
+ * Action: Create a new list and return the new list back to the user, including the id
+ */
+app.post("/lists/:listId/tasks", (req, res) => {
+  // get the task title that passed in the JSON body
+  let title = req.body.title;
+  // create new task within the passed in listId
+  let newTask = new Task({ title, _listId: req.params.listId });
+  // save the newList to MongoDB database then return that list to the client
+  newTask.save().then((taskDoc) => {
+    // the full task document is returned, including id
+    res.send(taskDoc);
+  });
+});
+
+/**
+ * PATCH /lists/:id
+ * Purpose: Update the list (PATCH request could be used to update PARTIAL list)
+ * Action: Update the old value with the new values specified in the JSON body of the request (no need to provide all fields)
+ */
+app.patch("/lists/:listId/tasks/:taskId", (req, res) => {
+  // The list information (fields) will be passed in via the JSON request body
+  // pass in the id, update the list that it found using the first param as the condition, with the req.body obj
+  Task.findOneAndUpdate(
+    { _id: req.params.taskId, _listId: req.params.listId },
+    { $set: req.body }
+  ).then((updatedList) => {
+    res.sendStatus(200); //send the status code of 200 which corresponds to a successful request.
+    // res.send(updatedList);
+  });
+});
+
+/**
+ * DELETE /lists/:id
+ * Purpose: Delete the record of the list
+ * Action: delete the record with the provided id in the url
+ */
+app.delete("/lists/:listId/tasks/:id", (req, res) => {
+  Task.findOneAndRemove(
+    { _id: req.params.taskId, _listId: req.params.listId },
+    { $set: req.body }
+  ).then((removedList) => {
+    res.send(removedList);
+  });
+});
+/** This is the end of Task routes **/
 // make it listen to the port 3000
 app.listen(3000, () => {
   console.log("Server is listening on port 3000");
