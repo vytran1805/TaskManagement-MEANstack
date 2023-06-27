739db2af23d3db8412d3df86aad0ef40630e3517
VyT <vytran1805@gmail.com>
2023-06-26 21:19:17 -0400

    Create New List Component

--- angular-frontend/src/app/app-routing.module.ts
+++ angular-frontend/src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { TaskViewComponent } from './pages/task-view/task-view.component';
+import { NewListComponent } from './pages/new-list/new-list.component';
 
-const routes: Routes = [{ path: '', component: TaskViewComponent }];
+const routes: Routes = [
+  { path: '', component: TaskViewComponent },
+  { path: 'new-list', component: NewListComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
